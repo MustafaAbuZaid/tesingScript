f7a182af635170336b587e6d9eabc6d8f044386d
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-03-08 23:06:54 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,6 @@
 
 function arenderjs_init(arenderjs_)
 {
-    // we register on current document change
-    arenderjs_.registerCurrentDocumentChangeEvent(function(id, title, metadata) 
-    {
-        armt_onCurrentDocumentChangeEvent(arenderjs_,id, title, metadata);
-              console.log(_id);
-    });
           getARenderJS().loadDocument("http://arender.arondor.com/pdf/pdf/PDFReference15_v5.pdf",
           function(id)
           {
