6fa4899d42472bbc369c10279c44e9f5493c690d
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-03-08 11:31:23 +0200

    testingScript
    testing the js calling

---
+++ test.js
@@ -0,0 +1,34 @@
+
+function arenderjs_init(arenderjs_)
+{
+	
+    window.alert("arenderjs_") 
+    window.alert(arenderjs_) 
+    // we register on current document change
+    arenderjs_.registerCurrentDocumentChangeEvent(function(id, title, metadata) 
+    {
+        armt_onCurrentDocumentChangeEvent(arenderjs_,id, title, metadata);
+    });
+}
+
+function armt_onCurrentDocumentChangeEvent(arenderjs_, id, title, metadata)
+{
+    var target="ARender samples111";
+    arenderjs_.getPageForNamedDestination(id,target, function(pageNumber) 
+    {
+
+        window.alert(arenderjs_) 
+        if (pageNumber==-1) 
+        {
+            window.alert("Could not find target named \""+ target + "\" are you sure this document contains it?");
+        } 
+        else
+        {
+            // desired position for the jump
+            var pos = arenderjs_.newPageRelativePosition(200,300,0,0);
+            // and now we jump! 
+            arenderjs_.askChangePage("Index", pageNumber, pos);
+        }
+    });
+}
+
