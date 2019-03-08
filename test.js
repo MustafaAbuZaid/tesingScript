7bc6e7392d3187139b19d3aeb4d38ac911284972
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-03-08 14:42:10 +0200

    Update test.js

--- test.js
+++ test.js
@@ -14,22 +14,4 @@ function arenderjs_init(arenderjs_)
           });  
 }	
 
-function armt_onCurrentDocumentChangeEvent(arenderjs_, id, title, metadata)
-{
-    var target="ARender samples111";
-    arenderjs_.getPageForNamedDestination(id,target, function(pageNumber) 
-    { 
-        if (pageNumber==-1) 
-        {
-           // window.alert("Could not find target named \""+ target + "\" are you sure this document contains it?");
-        } 
-        else
-        {
-            // desired position for the jump
-            var pos = arenderjs_.newPageRelativePosition(200,300,0,0);
-            // and now we jump! 
-            arenderjs_.askChangePage("Index", pageNumber, pos);
-        }
-    });
-}
 
