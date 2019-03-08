0e3fb8a27e83520ecf176e4d3fc34e4076fcb4ff
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-03-08 22:18:28 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,14 @@
 
 function arenderjs_init(arenderjs_)
 {
-     getARenderJS().registerNotifyLoadingErrorEvent(function(documentId,message) {console.log("error: "+message)});
- // Loads the PDF reference document
- // If an error occurs I am notified on the function defined before!
- getARenderJS().loadDocument("http://www.africau.edu/images/default/sample.pdf",
- function(id)
-     {
-      getARenderJS().openDocument(id);
-     });
+          getARenderJS().loadDocument("http://arender.arondor.com/pdf/pdf/PDFReference15_v5.pdf",
+          function(id)
+          {
+            getARenderJS().openDocument(id);
+          });
      // Loads the PDF reference document
      if(typeof arenderjs_ === 'object') { 
-      console.log("getARenderJS() --> ",getARenderJS()); 
-      console.log("arenderjs_.getDownloadDocumentJSAPI().askDownloadAllDocuments()");
-      console.log(arenderjs_.getDownloadDocumentJSAPI().askDownloadAllDocuments());
+      console.log("getARenderJS() --> ",getARenderJS());  
      }  
      console.log("done");
 }	
