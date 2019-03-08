8b0a5d111af5a16037a5fc27382007e3ea2ee27c
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-03-08 23:44:14 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 
 function arenderjs_init(arenderjs_)
 {
-  console.log(getARenderJS().loadDocument("http://arender.arondor.com/pdf/pdf/PDFReference15_v5.pdf"),function(id){console.log(id)});
+  console.log(getARenderJS().loadDocument("http://arender.arondor.com/pdf/pdf/PDFReference15_v5.pdf",function(id){console.log(id)}));
 //                                  ,
 //      function(id)
 //      {
