4963e6d8df58429a765ba92eca3d2fb576bc63d6
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-03-09 17:14:29 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ function arenderjs_init(arenderjs_)
 {
      getARenderJS().registerAllAsyncModulesStartedEvent(function() {
          // getARenderJS().openDocument("b64_dXJsPWh0dHA6Ly93d3cuYWZyaWNhdS5lZHUvaW1hZ2VzL2RlZmF1bHQvc2FtcGxlLnBkZg");
-       arenderjs_.loadDocument("http://192.168.0.175/?url=QWE.PNG",
+       arenderjs_.loadDocument("http://192.168.0.175/QWE.PNG",
 //      function(id)
 //      {
 //           arenderjs_.openDocument(id);
