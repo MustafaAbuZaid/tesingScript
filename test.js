fa4c06afc6790e111bdbfe3c9af0599cacdeef09
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-03-09 17:15:09 +0200

    Update test.js

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ function arenderjs_init(arenderjs_)
      getARenderJS().registerAllAsyncModulesStartedEvent(function() {
          // getARenderJS().openDocument("b64_dXJsPWh0dHA6Ly93d3cuYWZyaWNhdS5lZHUvaW1hZ2VzL2RlZmF1bHQvc2FtcGxlLnBkZg");
        arenderjs_.loadDocument("http://192.168.0.175/QWE.PNG",
-//      function(id)
-//      {
-//           arenderjs_.openDocument(id);
-//      }); 
+     function(id)
+     {
+          arenderjs_.openDocument(id);
+     }); 
           //console.log("id: ",arenderjs_.g.c.c);
      // Loads the PDF reference document
 //      if(typeof arenderjs_ === 'object') { 
