293c6076bff4668571f741a1b50cc097ede9211b
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-03-09 14:13:56 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ function arenderjs_init(arenderjs_)
 {
      getARenderJS().registerAllAsyncModulesStartedEvent(function() {
          // getARenderJS().openDocument("b64_dXJsPWh0dHA6Ly93d3cuYWZyaWNhdS5lZHUvaW1hZ2VzL2RlZmF1bHQvc2FtcGxlLnBkZg");
-       arenderjs_.loadDocument("http://arender.arondor.com/pdf/pdf/PDFReference15_v5.pdf",
-     function(id)
-     {
-          arenderjs_.openDocument(id);
-     }); 
+//        arenderjs_.loadDocument("http://arender.arondor.com/pdf/pdf/PDFReference15_v5.pdf",
+//      function(id)
+//      {
+//           arenderjs_.openDocument(id);
+//      }); 
           //console.log("id: ",arenderjs_.g.c.c);
      // Loads the PDF reference document
 //      if(typeof arenderjs_ === 'object') { 
