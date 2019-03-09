4c9865ccd85052dabce0dc8281b5012d81a34b15
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-03-09 12:52:50 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 function arenderjs_init(arenderjs_)
 {
      getARenderJS().registerAllAsyncModulesStartedEvent(function() {
+          getARenderJS().openDocument("b64_dXJsPWh0dHA6Ly93d3cuYWZyaWNhdS5lZHUvaW1hZ2VzL2RlZmF1bHQvc2FtcGxlLnBkZg");
      getARenderJS().loadDocument("http://arender.arondor.com/pdf/pdf/PDFReference15_v5.pdf",
      function(id)
      {
