fe305e48ffe31f756e989f75a14e127e2a3ee389
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-03-12 12:21:08 +0200

    Update test.js

--- test.js
+++ test.js
@@ -23,6 +23,21 @@ function arenderjs_init(arenderjs_)
 //      });
  
 }	
+function armt_onSubmitAlterDocumentContentEvent(arenderjs_,obj)
+{
+    var desc = arenderjs_.documentBuilder.getSubmittedAlterDocumentContentDescription(obj);
+    var meta = arenderjs_.documentBuilder.getDocumentMetadata(desc, 0);
+    arenderjs_.documentBuilder.addDocumentMetadata(meta, "name", "value"); 
+  arenderjs_.getDownloadDocumentJSAPI().askDownloadAllDocuments();
+}
+
+function armt_onNotifyAlterDocumentContentEvent(arenderjs_,obj)
+{
+    alert("Notify : " + obj);
+    var docId = arenderjs_.documentBuilder.getResultDocumentId(obj);
+    alert("Notify : " + docId);
+    console.log("Notify : docId = " + docId);
+}
 function followLink(docId, pageNumber, destination, action)
 {
   console.log(docId);
