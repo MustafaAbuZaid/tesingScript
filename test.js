465435f8d0a4d5d1e14d7e4441b28d9f356e29fa
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-03-12 12:23:45 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,11 +3,9 @@ var arenderjs;
 
 function arenderjs_init(arenderjs_)
 {
-             arenderjs_.onAnnotationModuleReady(function(annotjs)
-            {
-                arenderjs=annotjs;
-                annotjs.registerFollowLinkHandler(followLink); 
-            }); 
+     arenderjs_.documentBuilder.registerSubmitAlterDocumentContentEvent(function(obj){armt_onSubmitAlterDocumentContentEvent(arenderjs_,obj);});
+    arenderjs_.documentBuilder.registerNotifyAlterDocumentContentEvent(function(obj){armt_onNotifyAlterDocumentContentEvent(arenderjs_,obj);});
+
 //      getARenderJS().registerAllAsyncModulesStartedEvent(function() {
          // getARenderJS().openDocument("b64_dXJsPWh0dHA6Ly93d3cuYWZyaWNhdS5lZHUvaW1hZ2VzL2RlZmF1bHQvc2FtcGxlLnBkZg");
 //        arenderjs_.loadDocument("http://192.168.0.175/QWE.PNG",
@@ -28,7 +26,9 @@ function armt_onSubmitAlterDocumentContentEvent(arenderjs_,obj)
     var desc = arenderjs_.documentBuilder.getSubmittedAlterDocumentContentDescription(obj);
     var meta = arenderjs_.documentBuilder.getDocumentMetadata(desc, 0);
     arenderjs_.documentBuilder.addDocumentMetadata(meta, "name", "value"); 
-  arenderjs_.getDownloadDocumentJSAPI().askDownloadAllDocuments();
+  
+    alert("Notify : " + desc);
+    alert("Notify : " + meta);
 }
 
 function armt_onNotifyAlterDocumentContentEvent(arenderjs_,obj)
