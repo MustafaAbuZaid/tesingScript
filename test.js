fdde574f89347169eefa54901f319b213699b81d
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-03-12 10:26:13 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 
 function arenderjs_init(arenderjs_)
 {
+             arenderjs_.onAnnotationModuleReady(function(annotjs)
+            {
+                arenderjs=annotjs;
+                annotjs.registerFollowLinkHandler(followLink);
+                console.log(arenderjs.getDestinationTypes().b);
+                console.log(arenderjs.getActionTypes().b);
+            }); 
 //      getARenderJS().registerAllAsyncModulesStartedEvent(function() {
          // getARenderJS().openDocument("b64_dXJsPWh0dHA6Ly93d3cuYWZyaWNhdS5lZHUvaW1hZ2VzL2RlZmF1bHQvc2FtcGxlLnBkZg");
 //        arenderjs_.loadDocument("http://192.168.0.175/QWE.PNG",
@@ -14,9 +21,14 @@ function arenderjs_init(arenderjs_)
 //       console.log("getARenderJS() --> ",arenderjs_);  
 //      }   
 //      });
-
-
+ 
 }	
+function followLink(docId, pageNumber, destination, action)
+{
+    alert("docId=" + docId + ", pageNumber=" + pageNumber + ", dest=" + destination + ", action=" + action);
+    alert(arenderjs.getPropertyFromDestination(destination,"PAGE_TARGET"));
+    alert(arenderjs.getPropertyFromAction(action,"GOTO"));
+}
 
 
 
