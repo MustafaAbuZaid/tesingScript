0a16e634804d6f650320bc0b0c06145c3363557d
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-03-20 13:38:30 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,8 @@ function arenderjs_init(arenderjs_)
 {
       arenderjs_.loadDocument("http://192.168.0.175/9773665b-2106-4687-b97d-f74025fa625a.pdf",function(id)
 {
+     arenderjs_.getAnnotationJSAPI().add("b64_dXJsPWh0dHA6Ly8xOTIuMTY4LjAuMTc1LzI1N2EwZDg2LTAxZDgtNDQ3OS04ZDgwLTVhM2JkMTRmZjc5NS5wZGY",
+                                            "9773665b-2106-4687-b97d-f74025fa625a.pdf.xfdf");
     arenderjs_.openDocument(id);
 })
 
