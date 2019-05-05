1d9e070534871983dc423735ef1d8503ed36958e
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-05-05 12:52:53 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 
 function arenderjs_init(arenderjs_)
 { 
-      arenderjs_.loadDocument("http://192.168.0.175/577b050f-53b3-4f89-854e-a15508852220.png",function(id)
-{
-    arenderjs_.openDocument(id);
-})
+//       arenderjs_.loadDocument("http://192.168.0.175/577b050f-53b3-4f89-854e-a15508852220.png",function(id)
+// {
+//     arenderjs_.openDocument(id);
+// })
 
      console.log(arenderjs_);
      arenderjs_.registerAnnotationsSavedEvent(function(obj){armt_onSubmitAlterDocumentContentEvent(arenderjs_,obj);});
-
+topPanel.documentMenu.upload = false;
 }	
 function armt_onSubmitAlterDocumentContentEvent(arenderjs_,obj)
 {  
