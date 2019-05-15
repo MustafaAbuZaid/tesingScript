81fe3eb7fd938b9cb444a4e73f4f3fcff3c0afe2
Mustafa Abuzaid <46862050+MustafaAbuZaid@users.noreply.github.com>
2019-05-15 11:22:07 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,23 +1,23 @@
 
 function arenderjs_init(arenderjs_)
 { 
-//       arenderjs_.loadDocument("http://192.168.0.175/577b050f-53b3-4f89-854e-a15508852220.png",function(id)
-// {
-//     arenderjs_.openDocument(id);
-// })
+      arenderjs_.loadDocument("http://www.africau.edu/images/default/sample.pdf",function(id)
+{
+    arenderjs_.openDocument(id);
+})
 
-     console.log(arenderjs_);
+//      console.log(arenderjs_);
      arenderjs_.registerAnnotationsSavedEvent(function(obj){armt_onSubmitAlterDocumentContentEvent(arenderjs_,obj);});
 topPanel.documentMenu.upload = false;
 }	
 function armt_onSubmitAlterDocumentContentEvent(arenderjs_,obj)
 {  
-     console.log("Notify : " + obj);
-     console.log(arenderjs_);
-     console.log(arenderjs_.g.c);
-     arenderjs_.loadDocument(arenderjs_.g.c);
+//      console.log("Notify : " + obj);
+//      console.log(arenderjs_);
+//      console.log(arenderjs_.g.c);
+//      arenderjs_.loadDocument(arenderjs_.g.c);
       
-     arenderjs_.getDownloadDocumentJSAPI().askDownloadWithAnnotations(); 
+//      arenderjs_.getDownloadDocumentJSAPI().askDownloadWithAnnotations(); 
      //arenderjs_.getDownloadDocumentJSAPI().askDownloadAnnotations(); 
 } 
 
